ea9976a3f02c2aeda472b4f176e7f5812d115269
Jillian Nemec <jillian.nemec@nuancesolutions.com>
2021-03-27 18:29:15 -0500

    again

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -4,20 +4,22 @@ import scrape_mars
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
-def home():
-    mars_data = mongo.db.marsData.find_one()
-
-    return render_template("index.html", display_data=mars_data)
+def index():
+    mars = mongo.db.mars.find_one() 
+    return render_template('index.html', mars=mars)
 
 @app.route('/scrape')
 def scrape():
+    mars = mongo.db.mars
     mars_data = scrape_mars.scrape()
-    mongo.db.marsData.update({}, mars_data, upsert=True)
-
-    return redirect('/')
+    mars.update(
+        {},
+        mars_data,
+        upsert=True
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,10 +1,9 @@
-import pandas as pd
 from bs4 import BeautifulSoup as bs
-import requests
 from splinter import Browser
-import re
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas as pd
 
-mars_data = {}
 # ## NASA Mars News 
 # * Scrape the NASA [Mars News Site]('https://mars.nasa.gov/news/') and collect the latest News Title and Paragraph Text. Assign the text to variables that you can reference later.
 def init_browser():
@@ -18,22 +17,18 @@ def scrape():
     soup = bs(response.text, 'html.parser')
     news_title = soup.find('div', class_='content_title').find('a').text
     news_p = soup.find('div', class_='rollover_description_inner').text
-    mars_data['news_title']=news_title
-    mars_data['news_p']=news_p
 
 
 # ## JPL Mars Space Images - Featured Image
 
 
     image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-    browser.visit(image_url)
     image_html = browser.html
     image_soup = bs(image_html, 'html.parser')
     images = image_soup.find(class_="headerimage fade-in")
     image_string = images['src']
     image_base_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/'
     featured_image_url = image_base_url + image_string
-    mars_data['featured_image_url']= featured_image_url
 
 
 # ## Mars Facts
@@ -43,11 +38,10 @@ def scrape():
     mars_table.columns = ['Description','Value']
     mars_table.set_index('Description',inplace=True)
     marstable_html = mars_table.to_html()
-    mars_data['marstable_html']=marstable_html
 
 
 # ## Mars Hemispheres
-    browser.visit(mars_hemisphere_html)
+    mars_hemisphere_html = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     hemisphere_html = browser.html
     hemisphere_bs = bs(hemisphere_html,'html.parser')
 
@@ -61,11 +55,29 @@ def scrape():
         img_url = hemi_base + img_src
         hemisphere_image_urls.append({'title': title, 'img_url': img_url, 'img_src': img_src})
 
-    mars_data['hemisphere_image_urls'] =  hemisphere_image_urls
+
+    mars_data = { 
+        'news_title':news_title,
+        'news_p': news_p,
+        'feaured_image_url': featured_image_url,
+        'marstable_html': marstable_html,
+        'hemisphere_image_urls':hemisphere_image_urls
+    }
+
+    html = browser.html
+    report = bs(html, "html.parser")
+    mars_report = report.find_all("p")
+    mars_data["report"] = build_report(mars_report)
 
     browser.quit()
     return mars_data
 
+def build_report(mars_report):
+    final_report = ""
+    for p in mars_report:
+        final_report += " "+ p.get_text()
+        print(final_report)
+    return final_report
 
 
 
