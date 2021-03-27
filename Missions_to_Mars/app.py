0e3a98c83a1f3f734856dd6fbe262185302d4891
Jillian Nemec <jillian.nemec@nuancesolutions.com>
2021-03-27 16:00:19 -0500

    app.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+@app.route('/')
+def home():
+    mars_data = mongo.db.marsData.find_one()
+
+    return render_template("index.html", display_data=mars_data)
+
+@app.route('/scrape')
+def scrape():
+    mars_data = scrape_mars.scrape()
+    mongo.db.marsData.update({}, mars_data, upsert=True)
+
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
